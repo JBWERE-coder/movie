8e1c1a1a0e5d177796e8784db68bef37c8216e9b
JBWERE-code <lutherwere.com>
2024-02-19 23:09:49 +0300

    clean up & components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary components from react-router-dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import your components
+import MovieList from './components/MovieList/MovieList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
